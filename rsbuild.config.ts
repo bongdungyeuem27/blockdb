4150c1484f893ad4c373caf373bb770ddf0a3a9c
khanhlecavies <khanh@cavies.xyz>
2025-11-19 18:14:29 +0700

    chore: init tailwind

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+// import { tailwindcss } from '@tailwindcss/postcss';
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   plugins: [pluginReact()],
+  tools: {
+    postcss(config, { addPlugins }) {
+      // Add tailwindcss and autoprefixer to PostCSS plugins
+   
+      addPlugins([
+        // tailwindcss,
+      ]);
+      return config;
+    },
+  },
 });
